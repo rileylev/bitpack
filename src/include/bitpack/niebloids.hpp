f5ecf591c7db3085b14df559136174a80da815e6
Riley Levy <rileylevy95@gmail.com>
2020-08-29 00:16:18 -0400

    Rename niebloid macros with BITPACK_ prefix

--- src/include/bitpack/niebloids.hpp
+++ src/include/bitpack/niebloids.hpp
@@ -5,41 +5,40 @@
 
 #include <variant>
 
-namespace bitpack{
-// niebloids?
-#define FWD(x) std::forward<decltype(x)>(x)
+namespace bitpack {
+#define BITPACK_FWD(x) std::forward<decltype(x)>(x)
 #include <variant>
 namespace niebloids {
-#define NIEBLOID_(template_type, name, fn_name, struct_name)                   \
+#define BITPACK_NIEBLOID_(template_type, name, fn_name, struct_name)           \
   namespace impl {                                                             \
   using std::fn_name;                                                          \
   template<template_type T>                                                    \
   struct struct_name {                                                         \
     template<class... Args>                                                    \
     constexpr decltype(auto) operator()(Args&&... args)                        \
-        BITPACK_NOEXCEPT_WRAP(fn_name<T>(FWD(args)...));                       \
+        BITPACK_NOEXCEPT_WRAP(fn_name<T>(BITPACK_FWD(args)...));               \
   };                                                                           \
   }                                                                            \
   template<template_type T>                                                    \
   constexpr auto name = impl::struct_name<T>{};
-#define NIEBLOID(template_type, name, fn_name)                                 \
-  NIEBLOID_(template_type, name, fn_name, name##struct)
-NIEBLOID(class, get, get)
-NIEBLOID(auto, get_n, get)
-NIEBLOID(class, get_if, get_if)
-NIEBLOID(auto, get_if_n, get_if)
-NIEBLOID(class, holds_alternative, holds_alternative)
+#define BITPACK_NIEBLOID(template_type, name, fn_name)                         \
+  BITPACK_NIEBLOID_(template_type, name, fn_name, name##struct)
+BITPACK_NIEBLOID(class, get, get)
+BITPACK_NIEBLOID(auto, get_n, get)
+BITPACK_NIEBLOID(class, get_if, get_if)
+BITPACK_NIEBLOID(auto, get_if_n, get_if)
+BITPACK_NIEBLOID(class, holds_alternative, holds_alternative)
 
 namespace impl {
-  using std::visit;
+using std::visit;
 struct visit_struct {
   template<class... Args>
   constexpr decltype(auto) operator()(Args&&... args)
-      BITPACK_NOEXCEPT_WRAP(visit(FWD(args)...));
+      BITPACK_NOEXCEPT_WRAP(visit(BITPACK_FWD(args)...));
 };
 } // namespace impl
 [[maybe_unused]] constexpr impl::visit_struct visit{};
 } // namespace niebloids
-}
+} // namespace bitpack
 
 #endif
