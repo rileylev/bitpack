7b41b8c471df27f45c397f470bbe539905d1a3b1
Riley Levy <rileylevy95@gmail.com>
2020-08-29 17:55:29 -0400

    Make niebloids const-correct, rename s/get/get_t
    Considering how get<T> is not regular over pairs, I don't think it makes
    sense to bias the names to prefer it in general.

--- src/include/bitpack/niebloids.hpp
+++ src/include/bitpack/niebloids.hpp
@@ -15,17 +15,21 @@ namespace niebloids {
   template<template_type T>                                                    \
   struct struct_name {                                                         \
     template<class... Args>                                                    \
-    constexpr decltype(auto) operator()(Args&&... args)                        \
+    constexpr decltype(auto) operator()(Args&&... args) const                  \
         BITPACK_NOEXCEPT_WRAP(fn_name<T>(BITPACK_FWD(args)...));               \
   };                                                                           \
   }                                                                            \
   template<template_type T>                                                    \
   constexpr auto name = impl::struct_name<T>{};
 #define BITPACK_NIEBLOID(template_type, name, fn_name)                         \
-  BITPACK_NIEBLOID_(template_type, name, fn_name, name##struct)
-BITPACK_NIEBLOID(class, get, get)
+  BITPACK_NIEBLOID_(template_type, name, fn_name, name##_struct)
+
+// I don't think it's possible to have a template<class> struct and a
+// template<auto> struct with the same name.
+// t = by type, n = by index
+BITPACK_NIEBLOID(class, get_t, get)
 BITPACK_NIEBLOID(auto, get_n, get)
-BITPACK_NIEBLOID(class, get_if, get_if)
+BITPACK_NIEBLOID(class, get_if_t, get_if)
 BITPACK_NIEBLOID(auto, get_if_n, get_if)
 BITPACK_NIEBLOID(class, holds_alternative, holds_alternative)
 
