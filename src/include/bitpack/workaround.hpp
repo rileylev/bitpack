b8132b8562e2b0914d645bdbe3843e7dedb8ad78
Riley Levy <rileylevy95@gmail.com>
2020-09-12 01:35:41 -0400

    Postpone questions of && for later

--- src/include/bitpack/workaround.hpp
+++ src/include/bitpack/workaround.hpp
@@ -15,11 +15,10 @@ BITPACK_WRAP_STATIC(auto, get)
 BITPACK_WRAP_STATIC(class, holds_alternative)
 
 template<class R>
-inline constexpr auto visit(auto&& visitor, auto const self)
-    BITPACK_EXPR_BODY(decltype(self)::template visit<R>(BITPACK_FWD(visitor),
-                                                        self));
-inline constexpr auto visit(auto&& visitor, auto const self)
-    BITPACK_EXPR_BODY(decltype(self)::visit(BITPACK_FWD(visitor), self));
+inline constexpr auto visit(auto visitor, auto const self)
+    BITPACK_EXPR_BODY(decltype(self)::template visit<R>(visitor, self));
+inline constexpr auto visit(auto visitor, auto const self)
+    BITPACK_EXPR_BODY(decltype(self)::visit(visitor, self));
 
 } // namespace bitpack
 
